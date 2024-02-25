de424bb9c7534c1ae5fa6f3755d593ad679f9abf
Harshith S R <53342028+Harshith1234567@users.noreply.github.com>
2024-02-24 21:02:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Import langchain stuff 
 
 from langchain import PromptTemplate, LLMChain
-from langchain.llms import GPT4All
+from langchain.llms import GPT4All,
 from langchain.chains import SimpleSequentialChain
 from langchain.memory import ConversationBufferMemory
 
@@ -13,10 +13,14 @@ from langchain.memory import ConversationBufferMemory
 
 
 PATH = '/Users/admin/Library/Application Support/nomic.ai/GPT4All/mistral-7b-instruct-v0.1.Q4_0.gguf'
-
+PATH2= '/Users/admin/Documents/datascience/LLm/LLM-Models/codellama-7b-instruct.Q3_K_S.gguf'
 #stremlit Environment
 st.title('🐍 Python Co-Pilot 🤖')
 prompt = st.text_input('what code do you need')
+col1, col2 = st.columns([5,5]) 
+
+with col1:
+    model_name=st.selectbox('Model to be used',('Mistral Instruct','Code llama2'),index=0)
 
 
 code_templete = PromptTemplate(input_variables=['question'], 
@@ -28,9 +32,12 @@ memory= ConversationBufferMemory(input_key='question', memory_key='chat_history'
 llm = GPT4All(model=PATH, verbose=True)
 code_chain=LLMChain(prompt=code_templete, llm=llm, verbose=True, memory=memory)
 
+llama = GPT4All(model=PATH2, verbose=True)
+code2_chain=LLMChain(prompt=code_templete, llm=llama, verbose=True, memory=memory)
 
 
-if prompt: 
+
+if prompt and model_name=='Mistral Instruct': 
     
     response=code_chain.run(question = prompt)
     st.write(response)
@@ -38,41 +45,13 @@ if prompt:
     with st.expander('Message History'):
         st.info(memory.buffer)
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if prompt and model_name=='Code llama2': 
+    
+    response=code2_chain.run(question = prompt)
+    st.write(response)
+    
+    with st.expander('Message History'):
+        st.info(memory.buffer)
+    
 
 
